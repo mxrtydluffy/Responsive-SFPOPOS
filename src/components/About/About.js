46614bc64561555449c6fc7ef671d2a1fb8c82d7
mxrtydluffy <marty.marinda@students.dominican.edu>
2023-04-20 11:35:14 -0700

    [UPDATE] Responsiveness

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -3,9 +3,9 @@ import './About.css';
 
 function About() {
   return (
-    <div className='About'>
-      <h1>About SFPOPOS</h1>
-      <p>POPOS are publicly accessible spaces in
+    <div>
+      <h1 className='AboutTitle'>About SFPOPOS</h1>
+      <p className='AboutInfo'>POPOS are publicly accessible spaces in
         forms of plazas, terraces, atriums, small
         parks, and even snippets which are provided
         and maintained by private developers. In San
